7362406458b833e90af3f6c002020b81a9bcafdd
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-01-26 14:38:38 -0800

    chatterbox till fetching

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,36 @@
 // YOUR CODE HERE:
+
+var request = function(type, message) {
+	$.ajax({
+	  // This is the url you should use to communicate with the parse API server.
+	  url: 'http://parse.CAMPUS.hackreactor.com/chatterbox/classes/messages',
+	  type: type,
+	  data: message,
+	  contentType: 'application/json',
+	  success: function (data) {
+	    console.log('chatterbox: Message sent');
+	  },
+	  error: function (data) {
+	    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+	    console.error('chatterbox: Failed to send message', data);
+	  }
+	});
+}
+
+var app = {
+	init: () => {
+
+	},
+	send: (message) => {
+		request('POST', message);
+		// console.log($.ajax.args[0][0].data)
+		// var obj = JSON.parse($.ajax.args[0][0].data);
+		// console.log(obj)
+		// return obj;
+	},
+	fetch: (message) => {
+		request('GET', message);
+	}
+};
+
+
--- spec/chatterboxSpec.js
+++ spec/chatterboxSpec.js
@@ -46,6 +46,7 @@ describe('chatterbox', function() {
         app.send(message);
         ajaxOptions = typeof $.ajax.args[0][0] === 'object' ? $.ajax.args[0][0] : $.ajax.args[0][1];
         var result = ajaxOptions.data;
+        // console.log(result, message)
         expect(result).to.deep.equal(message);
         done();
       });
